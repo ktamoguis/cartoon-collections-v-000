21000ef03b15761b27d0b8c3a0945add18e83a77
Karl Amoguis <ktamoguis@gmail.com>
2018-04-27 05:27:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,17 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
   # Your code here
+  i = 0
+  while i < array.length
+    puts "#{i+1}. #{array[i]}"
+    i += 1
+  end
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
+  array.collect {|planet| planet.capitalize + "!"}
+
 end
 
 def long_planeteer_calls# code an argument here
